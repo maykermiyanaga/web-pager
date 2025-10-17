4662f523fad30d15b70c333075d15844c40d50e3
mayker <hydeki8@gmail.com>
2025-10-17 13:47:43 -0300

    initial commit

---
+++ src/main/java/com/maykermiyanaga/web_pager/WebPagerApplication.java
@@ -0,0 +1,13 @@
+package com.maykermiyanaga.web_pager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebPagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maykermiyanaga/web_pager/WebPagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maykermiyanaga.web_pager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
