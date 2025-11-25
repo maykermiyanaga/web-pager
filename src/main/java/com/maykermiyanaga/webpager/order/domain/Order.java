a3463ec4ab606d9dfbfa210c1efacb2d0205a63e
mayker <hydeki8@gmail.com>
2025-11-25 16:07:13 -0300

    feat: use UUIDv7 as primary key for Order entity

--- src/main/java/com/maykermiyanaga/webpager/order/domain/Order.java
+++ src/main/java/com/maykermiyanaga/webpager/order/domain/Order.java
@@ -1,11 +1,21 @@
 package com.maykermiyanaga.webpager.order.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.Getter;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.util.UUID;
 
 @Getter
 @Entity
 public class Order {
+
+    @Id
+    @UuidGenerator(style = UuidGenerator.Style.VERSION_7)
+    private UUID id;
+
     private String name;
+
     private String code;
 }
